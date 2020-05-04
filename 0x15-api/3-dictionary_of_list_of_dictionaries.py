5cc52c1571e280120bb05031220b230139bc2e9c
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-04 15:42:30 -0500

    [Add] Use .copy to fix the replace dictis

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Solve an API"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    import json
+
+    user_info = requests.get('https://jsonplaceholder.typicode.com/users')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+    
+    try:
+        tasks_json = tasks.json()
+        users = user_info.json()
+    except:
+        print("Not a valid JSON")
+        
+    task_dic = {}
+    task_list = []
+    id_dic = {}
+
+    for user in users:
+        name_employ = user.get("username")
+        for task in tasks_json:
+            if task.get("userId") is 1:
+                task_dic["username"] = name_employ
+                task_dic["task"] = task.get("title")
+                task_dic["completed"] = task.get("completed")
+                task_list.append(task_dic.copy())
+        id_dic[str(user.get("id"))] = task_list
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(id_dic, f)
