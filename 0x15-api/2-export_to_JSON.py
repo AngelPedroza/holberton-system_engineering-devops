2c3fbd924817193278c0decb7590e988de5e9d4e
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-04 20:25:26 -0500

    [Fix] f*ck

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,30 +11,30 @@ if __name__ == "__main__":
 
     try:
         user = user_info.json()
-        if user_id is user[0].get("id"):
-            name_employ = user[0].get("username")
     except:
         print("Not a valid JSON")
+    if user_id is user[0].get("id"):
+        name_employ = user[0].get("username")
 
     tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
                          .format(user_id))
 
     try:
         tasks_json = tasks.json()
-        complete = 0
-        task_dic = {}
-        task_list = []
-
-        for task in tasks_json:
-            task_dic["task"] = task.get("title")
-            task_dic["completed"] = task.get("completed")
-            task_dic["username"] = name_employ
-            task_list.append(task_dic.copy())
     except:
         print("Not a valid JSON")
+    complete = 0
+    task_dic = {}
+    task_list = []
+
+    for task in tasks_json:
+        task_dic["task"] = task.get("title")
+        task_dic["completed"] = task.get("completed")
+        task_dic["username"] = name_employ
+        task_list.append(task_dic.copy())
 
     id_dic = {}
     id_dic[str(user_id)] = task_list
 
-    with open(str(user_id) + ".json", "w") as f:
+    with open(str(user_id) + ".json", "w", newline="") as f:
         json.dump(id_dic, f)
