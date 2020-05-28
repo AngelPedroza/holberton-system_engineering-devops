cdfd883c1b1204632bb7fc2c07b37af82953d36d
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-28 17:53:27 -0500

    [Add] Add a correct path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,4 @@
 exec { '/bin/bash':
-  command => 'sed -i "s/worker_processes.*/worker_processes 10;/" /etc/nginx/nginx.conf',
-  path    => ['/usr/bin', '/usr/sbin'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  restart => 'sudo service nginx restart',
+  command => 'sed -i "s/worker_processes.*/worker_processes 10;/" /etc/nginx/nginx.conf ; sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
 }
