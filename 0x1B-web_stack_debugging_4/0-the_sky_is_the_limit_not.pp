2490efc1ee676b304a467ffb2d848dfc50ba5e09
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-28 17:54:28 -0500

    [Add] Add a comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# Add more workers
 exec { '/bin/bash':
   command => 'sed -i "s/worker_processes.*/worker_processes 10;/" /etc/nginx/nginx.conf ; sudo service nginx restart',
   path    => ['/usr/bin', '/bin'],
